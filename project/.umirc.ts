90aa036c322bccad8eaa7bbd580d805e5f2baa01
AJIA <zhujiasheng@users.noreply.github.com>
2020-01-20 00:44:53 +0800

    add project

--- project/.umirc.ts
+++ project/.umirc.ts
@@ -5,6 +5,7 @@ import plugins from './config/plugins';
 // ref: https://umijs.org/config/
 const config: IConfig =  {
   treeShaking: true,
+  exportStatic: true,
   hash:true,
   routes,
   plugins,
--- project/src/layouts/index.tsx
+++ project/src/layouts/index.tsx
@@ -2,26 +2,34 @@ import React from 'react';
 import BasicLayoutHead from './BasicLayoutHead';
 import BasicLayoutFoot from './BasicLayoutFoot';
 
-const BasicLayout: React.FC = props => {
-  const { pathname } = props.location;
-  let mytemlate = '';
-
-  switch (pathname) {
-   case '/login': mytemlate = <div> {props.children} </div>; 
-    break;
-   default : mytemlate = <div>
-      <BasicLayoutHead/>
-      {props.children}
-      {/* <BasicLayoutFoot/> */}
-    </div>
-    break;
+class BasicLayout extends React.Component {
+  constructor(props) {
+    super(props)
   }
 
-  return (
-    <div>
-     { mytemlate }
-    </div>
-  );
-};
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render () {
+    const { pathname } = this.props.location;
+    let mytemlate = '';
+  
+    switch (pathname) {
+     case '/login': mytemlate = <div> {this.props.children} </div>; 
+      break;
+     default : mytemlate = <div>
+        <BasicLayoutHead/>
+        {this.props.children}
+        {/* <BasicLayoutFoot/> */}
+      </div>
+      break;
+    }
+
+  return <div>{mytemlate}</div>
+  }
+}
 
 export default BasicLayout;
